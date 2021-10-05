3e52f382b62d852250bbb965feeb9c8adef82337
LIfe On The Murder Scene <84236806+jang-jinyeol@users.noreply.github.com>
2021-10-05 19:06:07 +0900

    Create image_load.py

---
+++ image_load.py
@@ -0,0 +1,23 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img_path = "IU.jpg"
+img = cv2.imread("D:\MyDataset\IU.jpg")
+
+# cv2.rectangle(img,(188,132),(471,522),(0,255,0),3)
+cv2.rectangle(img,(471,522),(188,132),(0,255,0),3)
+
+
+
+# opencv로 열기
+
+cv2.imshow(img_path,img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+# plt로 열기
+
+# plt.figure(figsize=(5,5))
+# plt.imshow(img[:,:,::-1])
+# plt.show()
