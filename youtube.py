75591a4e8b84d0857149a3925c3a9cff8bffa21b
LIfe On The Murder Scene <84236806+jang-jinyeol@users.noreply.github.com>
2021-10-08 15:52:00 +0900

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,4 +1,6 @@
 youtube-dl https://youtu.be/P3ln1nmbf3o
 
   
- # 위 예제는 오마이걸 영상임.
+# 위 예제는 오마이걸 영상임.
+# 조금 더 자세한 설명은 아래 링크
+# https://smilehugo.tistory.com/entry/how-to-download-youtube-videos-using-youtube-dl
