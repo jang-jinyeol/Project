bbb4050ce2ae7a464b4c499c0b65a9f1ce2e204a
LIfe On The Murder Scene <84236806+jang-jinyeol@users.noreply.github.com>
2021-10-09 23:45:41 +0900

    Update image_load.py

--- image_load.py
+++ image_load.py
@@ -9,11 +9,12 @@ cv2.rectangle(img,(471,522),(188,132),(0,255,0),3)
 
 
 
-#PIL로 열기
-
+# PIL로 열기
+# 크롤링한 이미지가 4채널이라 오류로 고생했는데 .convert('RGB')로 해결하였다.
+# opencv에선 안됨
 test = 'C:/Users/ddcfd/PycharmProjects/pythonProject/custom_dataset/train/아린/22JVB6VO7X_1.jpg'
 
-im2 = Image.open(test)
+im2 = Image.open(test).convert('RGB')
 im2.show()
 
 # opencv로 열기
