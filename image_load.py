638dbd4da64a3b0276467d90237d3f917c996553
LIfe On The Murder Scene <84236806+jang-jinyeol@users.noreply.github.com>
2021-10-09 23:05:52 +0900

    Update image_load.py

--- image_load.py
+++ image_load.py
@@ -1,6 +1,6 @@
 import cv2
 import matplotlib.pyplot as plt
-
+from PIL import Image
 img_path = "IU.jpg"
 img = cv2.imread(img_path)
 
@@ -8,6 +8,13 @@ img = cv2.imread(img_path)
 cv2.rectangle(img,(471,522),(188,132),(0,255,0),3)
 
 
+
+#PIL로 열기
+
+test = 'C:/Users/ddcfd/PycharmProjects/pythonProject/custom_dataset/train/아린/22JVB6VO7X_1.jpg'
+
+im2 = Image.open(test)
+im2.show()
 
 # opencv로 열기
 
