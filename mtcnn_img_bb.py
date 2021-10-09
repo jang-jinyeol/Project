b61f7e5e5fec8fbedd96836ffbcd067e6054478f
LIfe On The Murder Scene <84236806+jang-jinyeol@users.noreply.github.com>
2021-10-09 12:30:42 +0900

    Update mtcnn_img_bb.py

--- mtcnn_img_bb.py
+++ mtcnn_img_bb.py
@@ -13,6 +13,19 @@ class FaceDetector(object):
 
         boxes, probs, landmark =self.mtcnn.detect(img, landmarks=True)
         cv2.rectangle(img,(int(boxes[0][2]),int(boxes[0][3])),(int(boxes[0][0]),int(boxes[0][1])),(0,255,0),3)
+        
+'''
+# bb와 putText 그리는 함수
+def makeBox(voc_im,bbox,objects):
+  image = voc_im.copy()
+  for i in range(len(objects)):
+  cv2.rectangle(image,(int(bbox[i][0]),int(bbox[i][1])),(int(bbox[i][2]),int(bbox[i][3])),color = (0,255,0),thickness = 1)
+  cv2.putText(image, objects[i], (int(bbox[i][0]), int(bbox[i][1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2) # 크기, 색, 굵기
+  return image
+
+
+'''
+
         # cv2.rectangle(img, (471, 522), (188, 132), (0, 255, 0), 3)
         # print(int(boxes[0][2]))
         # print(boxes[0][3])
